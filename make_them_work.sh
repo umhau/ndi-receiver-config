2e78c9b53f928b8778cc21ebd9667cb78f134c64
um.hau <um.hau@outlook.com>
2020-06-22 15:51:09 -0400

    this should do it

--- make_them_work.sh
+++ make_them_work.sh
@@ -3,12 +3,12 @@
 # This is the comprehensive script, that combines all the others. It should not
 #  be used by itself, however, since it has no way to recover from errors.
 
-NDI_STREAM_NAME="$1"                 # pass the stream name to the shell script
+NDI_STREAM_NAME="${1@Q}"             # pass the stream name to the shell script
 STARTTIME=$(date +%s)
 TOO_LONG=180        # seconds before something went wrong starting the computer
 
 # turn on the computers, if they aren't already on
-ensure_reciever_is_on() { [ ! ping -c 1 "$1" ] && sudo etherwake -i enp0s7 -D "$2"; }
+ensure_reciever_is_on() { ! ping -c 1 "$1" && sudo etherwake -i enp0s7 -D "$2"; }
 ensure_reciever_is_on 192.168.1.112 
 ensure_reciever_is_on 192.168.1.80 
 ensure_reciever_is_on 192.168.1.82
@@ -28,6 +28,6 @@ done
 # play the NDI stream on each system. Note that the exact command is different
 # for some of the computers; thus, we use a wrapper script that we can 
 # customize locally.
-ssh ndi@192.168.1.112 "/home/ndi/play.sh $NDI_STREAM_NAME"
-ssh ndi@192.168.1.80  "/home/ndi/play.sh $NDI_STREAM_NAME"
-ssh ndi@192.168.1.82  "/home/ndi/play.sh $NDI_STREAM_NAME"
+ssh ndi@192.168.1.112 "bash /home/ndi/play.sh $NDI_STREAM_NAME &"
+ssh ndi@192.168.1.80  "bash /home/ndi/play.sh $NDI_STREAM_NAME &"
+ssh ndi@192.168.1.82  "bash /home/ndi/play.sh $NDI_STREAM_NAME &"
