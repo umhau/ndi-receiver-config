f7db9e275fd8bcd895ae82ec6443561a8858e48b
um.hau <um.hau@outlook.com>
2020-06-25 17:57:29 +0000

    sound works now.

--- play.sh
+++ play.sh
@@ -7,4 +7,4 @@ SOURCE="$1" # "${1@Q}"
 
 # this will have to be modified in some cases - e.g., where hdmi is not the 
 # audio channel being used.
-DISPLAY=':0' SDL_AUDIODRIVER="alsa" AUDIODEV='hdmi:CARD=PCH,DEV=0' /home/ndi/bin/ffplay -fs -f libndi_newtek -allow_video_fields false -i "$SOURCE"  > /dev/null 2>&1 < /dev/null
+DISPLAY=':0' SDL_AUDIODRIVER="alsa" AUDIODEV='hdmi:CARD=PCH,DEV=0' /home/ndi/bin/ffplay -af aresample=ocl=stereo -fs -f libndi_newtek -allow_video_fields false -i "$SOURCE"  > /dev/null 2>&1 < /dev/null
