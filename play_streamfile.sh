62a188b73954688f31bc63f01c1667c8c80ce6b9
um.hau <um.hau@outlook.com>
2020-06-25 18:04:36 +0000

    name correction

--- play_streamfile.sh
+++ play_streamfile.sh
@@ -6,4 +6,4 @@ set -v
 # run in the background.  The command string will have to be modified in some 
 # cases - e.g., where hdmi is not the audio channel being used.
 
-DISPLAY=':0' SDL_AUDIODRIVER="alsa" AUDIODEV='hdmi:CARD=PCH,DEV=0' /home/ndi/bin/ffplay -af aresample=ocl=stereo -fs -f libndi_newtek -allow_video_fields false -i `cat /home/ndi/streamfile.txt`  > /dev/null 2>&1 < /dev/null
+DISPLAY=':0' SDL_AUDIODRIVER="alsa" AUDIODEV='hdmi:CARD=PCH,DEV=0' /home/ndi/bin/ffplay -af aresample=ocl=stereo -fs -f libndi_newtek -allow_video_fields false -i `cat /home/ndi/streamname.txt`  > /dev/null 2>&1 < /dev/null
